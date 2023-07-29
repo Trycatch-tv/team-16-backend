21812d228317a05655f684c0cf8485c9758960ab
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-07-28 21:02:34 -0300

    Se refactorizo app

--- src/app.js
+++ src/app.js
@@ -3,27 +3,25 @@ import indexRoutes from "./routes/index.routes.js";
 import { config } from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
-import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer';
-import smtpTransport from "nodemailer-smtp-transport";
+import bodyParser from "body-parser";
 
 config();
-const tz = process.env.TZ || 'America/Argentina/Cordoba';
+const tz = process.env.TZ || "America/Argentina/Cordoba";
 const app = express();
-const entorno = process.env.NODE_ENV || 'dev';
-const url_front = process.env.HOST_FRONT || '*';
+const entorno = process.env.NODE_ENV || "dev";
+const url_front = process.env.HOST_FRONT || "*";
 
 let corsOptions = {
-    origin: url_front
+  origin: url_front,
 };
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(cors(corsOptions));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // parse requests of content-type - application/json
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,22 +29,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("env", entorno);
 app.set("port", process.env.API_PORT || 3000);
 app.use(express.json());
-app.set("port", process.env.API_PORT);
 app.use("/", indexRoutes);
 
 // Importamos los modelos
-import './models/user.model.js';
-import './models/role.model.js';
-import './models/log.model.js';
-import './models/provider.model.js';
-import './models/category.model.js';
-import './models/product.model.js';
-import './models/userrole.model.js';
-import './models/relations.model.js';
+import "./models/user.model.js";
+import "./models/role.model.js";
+import "./models/log.model.js";
+import "./models/provider.model.js";
+import "./models/category.model.js";
+import "./models/product.model.js";
+import "./models/userrole.model.js";
+import "./models/relations.model.js";
 
 app.use((req, res, next) => {
-    req.timezone = tz; // Establece la zona horaria deseada
-    next();
+  req.timezone = tz; // Establece la zona horaria deseada
+  next();
 });
 
 export default app;
